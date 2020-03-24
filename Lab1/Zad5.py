c8614c8f5fb801c5ac42b7da75bb38aec23fecfd
smekcio <mateuszsmekcio@gmail.com>
2020-03-24 03:13:27 +0100

    Prime number filter using comprehension list or map filter functions

---
+++ Lab1/Zad5.py
@@ -0,0 +1,14 @@
+def filtruj_pierwsze_v1(lista):
+    return list(map(lambda p: p**2 ,filter(lambda num: all(num%n != 0 for n in range(2,num)) and num>1, lista)))
+
+def filtruj_pierwsze_v2(lista):
+    return [num**2 for num in lista if all(num%n != 0 for n in range(2,num)) and num>1]
+
+def main():
+    lista = range(101)
+    #print(filtruj_pierwsze_v1(lista))
+    #print(filtruj_pierwsze_v2(lista))
+    print(filtruj_pierwsze_v1(lista)==filtruj_pierwsze_v2(lista))
+
+if __name__ == '__main__':
+    main()
